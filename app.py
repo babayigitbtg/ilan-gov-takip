35be983e3c11e08e5a02d559459592cefa561404
babayigitbtg <babayigitbtg@gmail.com>
2026-06-18 08:40:14 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import requests
+
+url = "https://www.ilan.gov.tr/ilan/kategori/9/ihale-duyurulari?aci=62&txv=9&field=publish_time&order=desc"
+
+r = requests.get(
+    url,
+    headers={
+        "User-Agent": "Mozilla/5.0"
+    },
+    timeout=30
+)
+
+print("STATUS:", r.status_code)
+print("=" * 80)
+print(r.text[:5000])
